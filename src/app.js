8fc8a1dd13b940e33df6e5803897523524ab2bc1
debimahapatra <mahapatradebi17@gmail.com>
2022-03-17 19:22:32 +0530

    weather tab with ip as well

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(express.static(publicDirPath))
 app.get('',(req,res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Debi Prasad Mahapatra'
+        name: ipvar
     })
 })
 
